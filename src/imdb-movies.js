5891452be136688e97ace739b835c2b14744a0fd
David <d.alekna@vetigraph.co.uk>
2017-05-04 11:52:17 +0100

    indent

--- src/imdb-movies.js
+++ src/imdb-movies.js
@@ -3,24 +3,24 @@ const fetch = require("node-fetch");
 class imdb {
 	async fetchImdbMovie(endpoint) {
 		const url = `https://www.omdbapi.com${endpoint}`;
-	    const response = await fetch(url);
-	    const json = await response.json();
+		const response = await fetch(url);
+		const json = await response.json();
 
-	    if (response.status !== 200)
-	        throw Error(json.message);
-
-	    return json;
+		if (response.status !== 200)
+		throw Error(json.message);
+		
+		return json;
 	}
 
 	async getByTitle(title, season = null, episode = null) {
 		try {
 			const Season = season ? `&Season=${season}` : "";
 			const Episode = episode ? `&Episode=${episode}` : "";
-	        const movie = await this.fetchImdbMovie(`/?t=${title}${Season}${Episode}`);
-	        console.log(movie);
-	    } catch (err) {
-	        console.error(`Error: ${err.message}`);
-	    }
+			const movie = await this.fetchImdbMovie(`/?t=${title}${Season}${Episode}`);
+			console.log(movie);
+		} catch (err) {
+			console.error(`Error: ${err.message}`);
+		}
 	}
 
 	async getById(id, season = null, episode = null) {
